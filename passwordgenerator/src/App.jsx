14ed3e02b8d7526e0a21da883ae556688f5ac4b2
badhra-danish <badhradanish24@gmail.com>
2025-05-13 12:16:23 +0530

    some change

--- passwordgenerator/src/App.jsx
+++ passwordgenerator/src/App.jsx
@@ -2,7 +2,6 @@ import { useState,useCallback,useEffect,useRef} from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { useEffect } from 'react'
 
 function App() {
   const [length, setlength] = useState(1)
